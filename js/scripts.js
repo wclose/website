20e2bf7b82dd1132d08e38c57a5583cab4b88b69
William L. Close <close.will@gmail.com>
2020-01-25 01:31:05 -0500

    setting back to old smooth scroll and removing pub link for now

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,10 @@ $(window).on("scroll", () => {
   }
 });
 
-function smoothScroll() {
-  preventDefault();
+$(".page-header .nav-link, .navbar-brand").on("click", function(e) {
+  e.preventDefault();
   const href = $(this).attr("href");
   $("html, body").animate({
     scrollTop: $(href).offset().top - 71
   }, 600);
-}
-
+});
