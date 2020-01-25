5389be02333148ed6a226c3a20c303cbebdd6bad
William L. Close <close.will@gmail.com>
2020-01-25 01:19:37 -0500

    trying to figure out scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,3 @@ $(window).on("scroll", () => {
     $body.removeClass(scrollClass);
   }
 });
-
-function smoothScroll(e) {
-  e.preventDefault();
-  const href = $(this).attr("href");
-  $("html, body").animate({
-    scrollTop: $(href).offset().top - 71
-  }, 600);
-}
-
-/*
-$(".page-header .nav-link, .navbar-brand").on("click", function(e) {
-  e.preventDefault();
-  const href = $(this).attr("href");
-  $("html, body").animate({
-    scrollTop: $(href).offset().top - 71
-  }, 600);
-});
-*/
