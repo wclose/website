c964f42185eb0081cf377a240d35b777c528edd6
William L. Close <close.will@gmail.com>
2020-01-26 01:04:51 -0500

    removing comment

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,3 @@ $("a[class='nav-link'][href^='#']").click(function(e) {
     scrollTop: $(href).offset().top - 71
   }, 600);
 });
-
-/*
-$(".page-header .nav-link, .navbar-brand").on("click", function(e) {
-  e.preventDefault();
-  const href = $(this).attr("href");
-  $("html, body").animate({
-    scrollTop: $(href).offset().top - 71
-  }, 600);
-});
-*/
